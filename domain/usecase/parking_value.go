f5dc76cc6f3912bd68d3d66caf327f3690f49080
Vanessa <vanessa.nunez.p@gmail.com>
2023-05-18 14:11:49 -0300

    feat: add function to calculate extra hour

--- domain/usecase/parking_value.go
+++ domain/usecase/parking_value.go
@@ -4,11 +4,18 @@ const weekdayParkValue float32 = 15.00
 const weekendParkValue float32 = 20.00
 
 func ParkingTotalValue(DateStart string, DateEnd string) float32 {
-	DateStartTime, DateEndTime := ConvertStringToDate(DateStart, DateEnd)
+	DateStartTime, DateEndTime := ConvertDBDateToDateTime(DateStart, DateEnd)
 	totalWeekEnd, totalWeekDay := CountWeekDayAndWeekEnd(DateStartTime, DateEndTime)
 	return CalculateParkingTotalValue(totalWeekDay, totalWeekEnd)
 }
 
 func CalculateParkingTotalValue(weekday int, weekend int) float32 {
 	return (float32(weekday) * weekdayParkValue) + (float32(weekend) * weekendParkValue)
+}
+
+func CalculateExtraParkingHour(extraHour int32) float32 {
+	if extraHour == 1 {
+		return weekdayParkValue
+	}
+	return 0
 }
--- domain/usecase/room_value.go
+++ domain/usecase/room_value.go
@@ -4,11 +4,18 @@ const weekdayRoomValue float32 = 120.00
 const weekendRoomValue float32 = 150.00
 
 func RoomTotalValue(DateStart string, DateEnd string) float32 {
-	DateStartTime, DateEndTime := ConvertStringToDate(DateStart, DateEnd)
+	DateStartTime, DateEndTime := ConvertDBDateToDateTime(DateStart, DateEnd)
 	totalWeekEnd, totalWeekDay := CountWeekDayAndWeekEnd(DateStartTime, DateEndTime)
 	return CalculateRoomTotalValue(totalWeekDay, totalWeekEnd)
 }
 
 func CalculateRoomTotalValue(weekday int, weekend int) float32 {
 	return float32(weekday)*weekdayRoomValue + float32(weekend)*weekendRoomValue
+}
+
+func CalculateExtraRoomHour(extraHour int32) float32 {
+	if extraHour == 1 {
+		return weekdayRoomValue
+	}
+	return 0
 }
