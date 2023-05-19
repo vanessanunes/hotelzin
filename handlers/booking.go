f764031f7034c5eda4391b71e0b762d70ce1cc3e
Vanessa <vanessa.nunez.p@gmail.com>
2023-05-18 23:04:25 -0300

    feat: add validated and check available dates to booking

--- handlers/booking.go
+++ handlers/booking.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"serasa-hotel/db"
 	"serasa-hotel/domain/repository"
+	"serasa-hotel/domain/usecase"
 	"serasa-hotel/models"
 	"serasa-hotel/response"
 )
@@ -19,13 +20,27 @@ func CreateBooking(w http.ResponseWriter, r *http.Request) {
 		response.ResponseError(w, http.StatusInternalServerError, err)
 		return
 	}
+	if err = booking.Validated(); err != nil {
+		response.ResponseError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	booked, err := usecase.CheckBookingAvailable(int32(booking.RoomID), booking.StartDatetime, booking.EndDatetime)
+	if err != nil {
+		log.Printf("Quarto %d disponivel!", booking.RoomID)
+	}
+
+	if booked != (models.Booking{}) {
+		log.Printf("Desculpe, o quarto para essa data já está ocupado. Por favor, tente outra data!")
+		response.ResponseJson(w, http.StatusConflict, "Desculpe, o quarto para essa data já está ocupado. Por favor, tente outra data!")
+		return
+	}
 
 	conn, err := db.OpenConnection()
 	if err != nil {
 		log.Println(err)
 	}
 	defer conn.Close()
-
 	repo := repository.ConnectionRepository(conn)
 	lastID, err := repo.InsertBooking(booking)
 
