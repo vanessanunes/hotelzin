18729a34ac27d9b147c769f2d21a4b77d724e3a6
Vanessa <vanessa.nunez.p@gmail.com>
2023-05-22 00:23:43 -0300

    chore: added swagger to main

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+//	@title			API de Hospedagem
+//	@version		1.0.0
+//	@contact.name	Vanessa Nunes
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+//	@host			localhost:9000
+//	@BasePath		/
 func main() {
 	err := configs.Load()
 
@@ -21,6 +28,7 @@ func main() {
 	routes.GetRoutes(r)
 
 	log.Println("listening...")
+	log.Println(fmt.Sprintf(":%s", configs.GetServerPort()))
 
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 
--- routes/router.go
+++ routes/router.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	_ "serasa-hotel/docs"
+
 	"github.com/go-chi/chi/v5"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func GetRoutes(r chi.Router) {
@@ -9,5 +12,9 @@ func GetRoutes(r chi.Router) {
 	r.Route("/booking", routesBooking)
 	r.Route("/checking", routesChecking)
 	r.Route("/checkout", routesCheckout)
+	r.Route("/payment", routesPayment)
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:9000/swagger/doc.json"), //The url pointing to API definition
+	))
 
 }
